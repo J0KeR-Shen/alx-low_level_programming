26b2d2ca083b510cda17e544b63ad89c413bac47
J0KeR-Shen <alejeuthman@gmail.com>
2023-02-18 17:13:04 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main - NTRY POINT
+ * main - ENTRY POINT
  * Return:0
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
