388fc264594404c8c66643ebb6fce68b2b5cd109
J0KeR-Shen <alejeuthman@gmail.com>
2023-02-19 04:19:41 -0800

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,28 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
- * Description: This function prints the message "and that piece of art is useful"
- * to the standard error, followed by a new line, using the write system call.
- *
- * Return: Always 1 (Error)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-size_t length = strlen(message);
-ssize_t written = write(STDERR_FILENO, message, length);
-
-if (written == -1) {
-perror("Error");
-exit(errno);
-}
-
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
-
